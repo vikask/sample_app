de34285e804d871d8effa11eaa71052de6381111
pht-20 <pht-20@pht20-Inspiron-580s.(none)>
2013-01-16 15:59:34 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '7cee3a95f3a615e8d14d4a106a385d0da819aea4dcac51ff1566d10febd5972239e5b0c41f15a52465d771ce0fc2ebc037fffa3d51b2449c6f6f36a26c7b1ce4'
